caae22e28cf109b614d2c46b369ac3705d81186a
Joshua Carpeggiani <32605850+joshcarp@users.noreply.github.com>
2020-07-29 18:22:56 +1000

    Feature/grpc pattern (#45)
    * Add ~gRPC pattern to application
    
    * Change GRPC and grpc for gRPC pattern on applications

--- proto2sysl/main.go
+++ proto2sysl/main.go
@@ -57,6 +57,7 @@ func VisitService(importPrefix string, module *sysl.Module, s *protogen.Service)
 	app.Attrs["package"] = newsysl.Attribute(pkgName)
 	app.Attrs["source_path"] = newsysl.Attribute(path.Join(importPrefix, s.Location.SourceFile))
 	app.Attrs["description"] = newsysl.Attribute(cleanDescription(s.Comments))
+	app.Attrs["patterns"] = newsysl.Pattern("gRPC")
 	module.Apps[name] = app
 	for _, e := range s.Methods {
 		if err := VisitMethod(importPrefix, module, e); err != nil {
@@ -87,7 +88,7 @@ func VisitMethod(importPrefix string, module *sysl.Module, m *protogen.Method) e
 	// Attributes
 	endpoint.Attrs = make(map[string]*sysl.Attribute)
 	endpoint.Attrs["description"] = newsysl.Attribute(cleanDescription(m.Comments))
-	endpoint.Attrs["patterns"] = newsysl.Pattern("grpc", "GRPC")
+	endpoint.Attrs["patterns"] = newsysl.Pattern("gRPC")
 	endpoint.Attrs["source_path"] = newsysl.AttributeAny(path.Join(importPrefix, m.Location.SourceFile))
 
 	module.Apps[appName].Endpoints[endpointName] = endpoint
